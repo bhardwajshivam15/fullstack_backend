c57f462c40a5e2687f3f784b6c307d8c2ddb2c27
Shivam Bhardwaj <shivam.bhardwaj@tbo.com>
2024-08-13 16:12:08 +0530

    get request mapping

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
+
+    @GetMapping("/home")
+    public String Home(){
+        return "Home";
+    }
 }
